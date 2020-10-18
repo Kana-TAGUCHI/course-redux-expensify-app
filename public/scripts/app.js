c777633eaaf1589890e7abb89c26d8929a130786
kana-wwib <mukrabwy103@gmail.com>
2020-10-18 16:06:12 +0900

    section 1-3 completed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,40 +1,38 @@
 'use strict';
 
-// argument objects - no longer bound with arrow functions
+var visibility = false;
 
-var add = function add(a, b) {
-  // console.log(arguments);
-  return a + b;
-};
-console.log(add(55, 1));
-
-// this keyword - no longer bound 
-
-var user = {
-  name: 'Andrew',
-  cities: ['Philadelphia', 'New York', 'Dublin'],
-  printPlaceLived: function printPlaceLived() {
-    var _this = this;
-
-    return this.cities.map(function (city) {
-      return _this.name + ' has lived in ' + city;
-    });
-  }
+var toggleVisibility = function toggleVisibility() {
+  visibility = !visibility;
+  render();
 };
 
-console.log(user.printPlaceLived());
+var render = function render() {
+  var jsx = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Visibility Toggle'
+    ),
+    React.createElement(
+      'button',
+      { onClick: toggleVisibility },
+      visibility ? 'Hide details' : 'Show details'
+    ),
+    visibility && React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'p',
+        null,
+        'Hey. These are some details you can now see!'
+      )
+    )
+  );
 
-// Challenge
-var multiplier = {
-  numbers: [10, 20, 30],
-  multiplyBy: 3,
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function (number) {
-      return number * _this2.multiplyBy;
-    });
-  }
+  ReactDOM.render(jsx, document.getElementById('app'));
 };
 
-console.log(multiplier.multiply());
+render();
--- src/app.js
+++ src/app.js
@@ -3,40 +3,57 @@ console.log('App.js is running!');
 const app = {
   title: 'Indecision App',
   subtitle: 'This is some info',
-  options: ['One', 'Two'],
+  options: []
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
 }
 
-var template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    {(app.options.length > 0) ? 'Here are your options' : 'No Options'}
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
-);
-
-const user = {
-  name: 'Kana',
-  age: 27,
-  location: 'Japan'
+const onRemoveAll = () => {
+  app.options = [];
+  render();
 }
-function getLocation(location) {
-  if (location) {
-  return <p>location: {location}</p>;
-  } 
-}
-const templateTwo = (
-  <div>
-  <h1>{user.name ? user.name : 'Anonymous'}</h1>
-  {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-  {getLocation(user.location)}
-  </div>
-);
 
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+}
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        {
+          app.options.map((option) => {
+            return <li key={option}>{option}</li>
+          })
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"></input>
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  
+  ReactDOM.render(template, appRoot);
+}
+
+render();
